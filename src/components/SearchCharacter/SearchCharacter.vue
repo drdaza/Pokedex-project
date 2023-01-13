f161882b21fc7f2ed4f50d2183ccb6a566ff9f64
David Daza <drdaza18@gmail.com>
2023-01-13 14:17:37 +0100

    added searchcharacter component

---
+++ src/components/SearchCharacter/SearchCharacter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+const search = ref('')
+
+const emits = defineEmits(['NameEmit'])
+
+const emitSearch = ()=>{
+    emits('NameEmit', search.value)
+}
+</script>
+<template>
+    <input type="text" v-model="search">
+    <button @click="emitSearch">search pokemon</button>
+</template>
+<style lang="scss" scoped>
+    
+</style>
